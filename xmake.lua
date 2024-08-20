15777f50943e67c15f8d04e2ada4a6bac41917fe
ShelpAm <shelpam@qq.com>
2024-08-20 23:49:08 +0800

    build: remove third-party instead managed by xmake

--- xmake.lua
+++ xmake.lua
@@ -1,15 +1,13 @@
 set_languages("cxxlatest")
 
-add_requires("spdlog")
+add_requires("spdlog", "asio")
 
 target("little-sb-client")
 set_kind("binary")
 add_files("src/little-sb-client.cpp")
-add_includedirs("third-party/asio-1.30.2/include")
-add_packages("spdlog")
+add_packages("spdlog", "asio")
 
 target("little-sb-server")
 set_kind("binary")
 add_files("src/little-sb-server.cpp")
-add_includedirs("third-party/asio-1.30.2/include")
-add_packages("spdlog")
+add_packages("spdlog", "asio")
